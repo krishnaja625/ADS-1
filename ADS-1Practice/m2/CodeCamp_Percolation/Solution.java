e0914aad930035979c4bab30d139b592c8a4d642
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-25 18:03:39 +0530

    "check_style_score -> 6 of 24 passed. style score: 0/1 "

---
+++ ADS-1Practice/m2/CodeCamp_Percolation/Solution.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+class per {
+	int[][] grid;
+	int cnt;
+	int size;
+	Weightedunion cd;
+	per(int size) {
+		grid = new int[size][size];
+		cnt = 0;
+		cd = new Weightedunion((size*size) +2);
+		size = size;
+	}
+	void open(int row, int col) {
+		grid[row-1][col - 1] = 1;
+		cnt++;
+		if(row==0) {
+			cd.union(0, component(row-1, col-1));
+		}
+		if (row == size -1) {
+			cd.union((size*size) +1 , component(row-1, col-1));
+		}
+		if (row>0 && row <= size - 1) {
+			if(grid[row][col] == 1) {
+				cd.union(component(row - 1, col) , component(row, col));
+			}
+		}
+		if (col>0 && row <= size - 1) {
+			if(grid[row-1][col+1] == 1) {
+				cd.union(component(row - 1, col) , component(row, col));
+			}
+		}	
+	}
+	int component(int i, int j) {
+		return (i*size+ j)+1;
+	}
+	boolean isopen(int row, int col) {
+		return grid[row-1][col - 1] == 1;
+	}
+	boolean isFull(int row, int col) {
+		return grid[row-1][col - 1] == 0;
+	}
+	int numberofopensites() {
+		return cnt;
+	}
+	boolean percolates() {
+		return cd.connected(0, (size*size) +1);
+
+	}
+}
+class Solution {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		per wc = new per(n);
+		while(s.hasNext()){
+		wc.open(s.nextInt(), s.nextInt());	
+		}	
+		System.out.println(wc.percolates());
+}
+}
