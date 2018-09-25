59444bc6d53d3c079af375bda3cb945c9860c48f
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-25 20:09:24 +0530

    "check_style_score -> 22 of 24 passed. style score: 0/1 "

--- ADS-1Practice/m2/CodeCamp_Percolation/Solution.java
+++ ADS-1Practice/m2/CodeCamp_Percolation/Solution.java
@@ -3,134 +3,137 @@ import java.util.Scanner;
 /**
  * Class for percolation.
  */
-class percolation {
+class Percolation {
 /**
  * variable to keep count value.
  */
-	private int cnt;
-	/**
-	 * to take 2 dimentional array.
-	 */
-	private int[][] grid;
-	/**
-	 * { to keep size }.
-	 */
-	private int size;
-	/**
-	 * object for weighted union class.
-	 */
-	Weightedunion cd;
+    private int cnt;
+    /**
+     * to take 2 dimentional array.
+     */
+    private int[][] grid;
+    /**
+     * { to keep size }.
+     */
+    private int size;
+    /**
+     * object for weighted union class.
+     */
+    private Weightedunion cd;
 /**
  * Constructs the object.
  *
  * @param      size  The size
  */
-	percolation(final int size) {
-		grid = new int[size][size];
-		cnt = 0;
-		cd = new Weightedunion((size * size) + 2);
-		this.size = size;
-	}
-	/**
-	 * function to check whether the element has to be connected or not.
-	 *
-	 * @param      row   The value of row
-	 * @param      col   The value of column
-	 */
-	void open(final int row, final int col) {
-		grid[row][col] = 1;
-		cnt++;
-		if(row == 0) {
-			cd.union(0, component(row, col));
-		}
-		if (row == size - 1) {
-			cd.union((size*size) + 1 , component(row, col));
-		}
-		if(row + 1 < size && grid[row][col] == 1) {
-				cd.union(component(row + 1, col) , component(row, col));
-			}
-		if (row - 1 > 0 && grid[row - 1][col] == 1) {
-				cd.union(component(row - 1, col) , component(row, col));
-		}
-		if (col - 1 > 0 && grid[row][col - 1] == 1) {
-				cd.union(component(row, col - 1) , component(row, col));
-		}	
-		if (col + 1 < size && grid[row][col + 1] == 1) {
-				cd.union(component(row, col + 1) , component(row, col));
-		}
-	}
-	/**
-	 * to get the component at the particular row and column.
-	 *
-	 * @param      i     row index is given.
-	 * @param      j     column index is given
-	 *
-	 * @return     return type is int
-	 */
-	int component(final int i, final int j) {
-		return (i) * size + j ;
-	}
-	/**
-	 * Determines if open.
-	 *
-	 * @param      row   The row
-	 * @param      col   The col
-	 *
-	 * @return     True if open, False otherwise.
-	 */
-	boolean isOpen(final int row, final int col) {
-		return grid[row][col] == 1;
-	}
-	/**
-	 * Determines if full.
-	 *
-	 * @param      row   The row
-	 * @param      col   The col
-	 *
-	 * @return     True if full, False otherwise.
-	 */
-	boolean isFull(final int row, final int col) {
-		return grid[row][col] == 0;
-	}
-	/**
-	 * to get the number of open sites.
-	 *
-	 * @return     integer is returned.
-	 */
-	int numberofopensites() {
-		return cnt;
-	}
-	/**
-	 * function to check if the grid percolates or not.
-	 *
-	 * @return     True if percolates, False otherwise.
-	 */
-	boolean percolates() {
-		return cd.connected(0, (size*size) + 1);
-	}
+    Percolation(final int size) {
+        grid = new int[size][size];
+        cnt = 0;
+        cd = new Weightedunion((size * size) + 2);
+        this.size = size;
+    }
+    /**
+     * function to check whether the element has to be connected or not.
+     *
+     * @param      row   The value of row
+     * @param      col   The value of column
+     */
+    void open(final int row, final int col) {
+        grid[row][col] = 1;
+        cnt++;
+        if (row == 0) {
+            cd.union(0, component(row, col));
+        }
+        if (row == size - 1) {
+            cd.union((size * size) + 1, component(row, col));
+        }
+        if (row + 1 < size && grid[row][col] == 1) {
+                cd.union(
+                    component(row + 1, col), component(row, col));
+            }
+        if (row - 1 > 0 && grid[row - 1][col] == 1) {
+                cd.union(
+                    component(row - 1, col), component(row, col));
+        }
+        if (col - 1 > 0 && grid[row][col - 1] == 1) {
+                cd.union(component(row, col - 1), component(row, col));
+        }
+        if (col + 1 < size && grid[row][col + 1] == 1) {
+                cd.union(
+                    component(row, col + 1), component(row, col));
+        }
+    }
+    /**
+     * to get the component at the particular row and column.
+     *
+     * @param      i     row index is given.
+     * @param      j     column index is given
+     *
+     * @return     return type is int
+     */
+    int component(final int i, final int j) {
+        return (i) * size + j;
+    }
+    /**
+     * Determines if open.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if open, False otherwise.
+     */
+    boolean isOpen(final int row, final int col) {
+        return grid[row][col] == 1;
+    }
+    /**
+     * Determines if full.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if full, False otherwise.
+     */
+    boolean isFull(final int row, final int col) {
+        return grid[row][col] == 0;
+    }
+    /**
+     * to get the number of open sites.
+     *
+     * @return     integer is returned.
+     */
+    int numberofopensites() {
+        return cnt;
+    }
+    /**
+     * function to check if the grid percolates or not.
+     *
+     * @return     True if percolates, False otherwise.
+     */
+    boolean percolates() {
+        return cd.connected(0, (size * size) + 1);
+    }
 }
 /**
  * Class for solution.
  */
 class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
+    /**
+     * Constructs the object.
+     */
+    Solution() {
 
-	}
-	/**
-	 * function of main.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		percolation wc = new percolation(n);
-		while (s.hasNext()) {
-		wc.open(s.nextInt() - 1, s.nextInt() - 1);
-		}
-		System.out.println(wc.percolates());
+    }
+    /**
+     * function of main.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        Percolation wc = new Percolation(n);
+        while (s.hasNext()) {
+        wc.open(s.nextInt() - 1, s.nextInt() - 1);
+        }
+        System.out.println(wc.percolates());
 }
 }
