081766815af7ba55a19f4e4d00f7deb5569d4165
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-26 19:18:23 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- ADS-1Practice/m3/3-sum/Solution.java
+++ ADS-1Practice/m3/3-sum/Solution.java
@@ -13,15 +13,10 @@ for(int i= 0; i<arr.length; i++) {
 }
 int cnt =0;
 Arrays.sort(arr);
-/*if (z == -1) {
-  System.out.println("element not present in the array");
-} else {*/
   for(int i = 0; i < arr.length && arr[i] <0; i++) {
     for (int j = i+1; j < arr.length && arr[i] + arr[j] <0; j++) {
       int a = -1 * (arr[i] + arr[j]);
-      int z = binarySearch(arr, 0, arr.length-1, a);
-/*      System.out.println((arr[i] + arr[j]) + " " +a);
-    System.out.println( binarySearch(arr, 0, arr.length-1, a));*/ ;
+      int z = binarySearch(arr, j+1, arr.length-1, a);
       if (z > j) {
         cnt++;  
     }
