9e7c75c26c646449fb8fcc4b0e40142876ed646c
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-26 18:50:58 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS-1Practice/m3/3-sum/Solution.java
+++ ADS-1Practice/m3/3-sum/Solution.java
@@ -19,7 +19,7 @@ Arrays.sort(arr);
   for(int i = 0; i < arr.length; i++) {
     for (int j = i+1; j < arr.length; j++) {
       int a = -1 * (arr[i] + arr[j]);
-      int z = binarySearch(arr, 0, arr.length, a);
+      int z = binarySearch(arr, 0, arr.length-1, a);
 /*      System.out.println((arr[i] + arr[j]) + " " +a);
     System.out.println( binarySearch(arr, 0, arr.length-1, a));*/ ;
       if (z > j) {
