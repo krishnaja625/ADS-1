2e4f33ca767e9ef5f04af671d881495f336ea94d
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-26 21:54:53 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS-1Practice/m3/Sorted_Array/Solution.java
+++ ADS-1Practice/m3/Sorted_Array/Solution.java
@@ -1,5 +1,26 @@
-import java.util.*;
+/**
+ * To import Scanner.
+ */
+import java.util.Scanner;
+/**
+ * To import Arrays class.
+ */
+import java.util.Arrays;
+/**
+ * Class for solution.
+ */
 class Solution {
+/**
+ * Constructs the object.
+ */
+    protected Solution() {
+
+    }
+    /**
+     * function for main.
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int size1 = Integer.parseInt(input.nextLine());
@@ -9,47 +30,41 @@ class Solution {
         int[] array1;
         int[] array2;
         array2 = new int[arr2.length];
-
-/*        if(t[0].equals("")) {
-            array1 = new int[0];
-        }
-        else {*/
             array1 = new int[size1];
             for(int i = 0; i<size1; i++) {
             array1[i] = Integer.parseInt(arr1[i]);
-       /* }*/
         }
-        for(int i = 0; i<arr2.length; i++) {
+        for(int i = 0; i < arr2.length; i++) {
             array2[i] = Integer.parseInt(arr2[i]);
         }
-
-
-        // System.out.println(Arrays.toString(array1));
-        // System.out.println(Arrays.toString(array2));
         sortedArray(array1, array2);
-
     }
+/**
+ * function for sorting the given arrays.
+ *
+ * @param      array1  The array 1
+ * @param      array2  The array 2
+ */
 
-    public static void sortedArray(int[] array1, int[] array2) {
+    public static void sortedArray(final int[] array1, final int[] array2) {
         int i = 0;
         int j = 0;
         int [] result;
         result = new int[array1.length + array2.length];
-        for(int a = 0; a < result.length; a++) {
+        for (int a = 0; a < result.length; a++) {
             if (i < array1.length && j < array2.length) {
-                if(array1[i] < array2[j]) {
+                if (array1[i] < array2[j]) {
                     result[a] = array1[i++];
-                }
-                else
+                } else {
                     result[a] = array2[j++];
-            }
-            else if(i>=array1.length) {
+                }
+            } else if (i >= array1.length) {
                 result[a] = array2[j++];
-            }
-            else if(j>=array2.length) {
+            } else if (j >= array2.length) {
                 result[a] = array1[i++];
             }
         }
-        System.out.println(Arrays.toString(result).replace("[", "").replace("]","").replace(" ",""));
+        System.out.println(Arrays.toString(
+            result).replace("[", "").replace("]", "").replace(" ", ""));
     }
 }
