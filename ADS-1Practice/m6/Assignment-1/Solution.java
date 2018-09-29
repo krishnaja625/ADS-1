45a609cceded0faa4834f1e9fbba54f7a8fa9e88
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-29 11:41:32 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS-1Practice/m6/Assignment-1/Solution.java
+++ ADS-1Practice/m6/Assignment-1/Solution.java
@@ -129,6 +129,8 @@ class AddLargeNumbers {
 				s1.push(elemnt1.data);
 				elemnt1 = elemnt1.next;
 			}
+			System.out.println(s1.pop());
+			System.out.println(s1.pop());
 			for(int i = 0; i < list1.size; i++) {
 				s2.push(elemnt2.data);
 				elemnt2 = elemnt2.next;
