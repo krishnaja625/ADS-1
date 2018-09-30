0f6695edb70c8d158cc6044b531992754f0564b2
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-30 22:02:46 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- ADS-1Practice/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1Practice/m4/4.1 Balanced Paranthesis/Solution.java
@@ -9,17 +9,17 @@ class Solution {
     /**
      * Constructs the object.
      */
-    Solution() {
+    protected Solution() {
     }
     /**
      * Function for main.
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        while(n != 0) {
+        while (n != 0) {
             String[] input = sc.nextLine().split("");
             Balanced b = new Balanced();
             if (b.balancing(input)) {
