fe7690dce3e870717264835a308f939dd1348276
krishnaja625 <krishnaja625@msitprogram.net>
2018-10-01 12:12:11 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS-1Practice/m7/7.1 Steque/Solution.java
+++ ADS-1Practice/m7/7.1 Steque/Solution.java
@@ -1,201 +1,48 @@
-/**
- * { item_description }
- */
 import java.util.Scanner;
 /**
- * Class for node.
+ * Class for solution.
  */
-class Node {
-    String data;
-    Node next;
+public final class Solution {
     /**
      * Constructs the object.
      */
-    Node() {
-    }
-    /**
-     * Constructs the object.
-     *
-     * @param      data  The data
-     */
-    Node(final String data) {
-        this.data = data;
-    }
-}
-/**
- * List of linkeds.
- */
-class LinkedList {
-    Node first;
-    Node last;
-    private int size;
-    /**
-     * Constructs the object.
-     */
-    LinkedList() {
-        first = new Node();
-        last = new Node();
-        size = 0;
-    }
-    /**
-     * { function_description }.
-     *
-     * @param      item  The item
-     */
-    void insert(final String item) {
-        Node obj = new Node(item);
-        if (size == 0) {
-            first = obj;
-            last = obj;
-            size++;
-            return;
-        }
-        last.next = obj;
-        // last = obj;
-        last = last.next;
-
-        size++;
-    }
-    /**
-     * { function_description }.
-     *
-     * @param      data  The data
-     */
-    public void insertFirst(final String data) {
-        Node head = new Node(data);
-        if (size == 0) {
-            first = head;
-            size++;
-            return;
-        }
-        head.next = first;
-        first = head;
-        size++;
-    }
-    /**
-     * { function_description }.
-     *
-     * @return     { description_of_the_return_value }
-     */
-    String delete() {
-        String data = first.data;
-        first = first.next;
-        size--;
-        return data;
-    }
-    void print() {
-        if (size == 0) {
-            System.out.println("Steque is empty.");
-        }
-        Node tmp = first;
-        while (tmp.next != null) {
-            System.out.print(tmp.data + ", ");
-            tmp = tmp.next;
-        }
+    private Solution() {
 
     }
     /**
-     * Determines if empty.
-     *
-     * @return     True if empty, False otherwise.
+     * main method to drive the program.
+     * @param      args  The arguments
      */
-    boolean isEmpty() {
-        if (size == 0) {
-            return true;
-        }
-        return false;
-    }
-    /**
-     * { function_description }.
-     *
-     * @return     { description_of_the_return_value }
-     */
-    int size() {
-        return size;
-    }
-}
-/**
- * Class for stack.
- */
-class Stack {
-    /**
-     * { var_description }.
-     */
-    LinkedList llist;
-    /**
-     * Constructs the object.
-     */
-    Stack() {
-        llist = new LinkedList();
-    }
-    /**
-     * { function_description }.
-     *
-     * @param      item  The item
-     */
-    void push(final String item) {
-        llist.insertFirst(item);
-    }
-    void enqueue(final String item) {
-        llist.insert(item);
-    }
-    /**
-     * { function_description }.
-     *
-     * @return     { description_of_the_return_value }
-     */
-    String pop() {
-        if (llist.size() == 0) {
-            System.out.println("Steque is empty.");
-        }
-        llist.print();
-        return llist.delete();
-    }
-    /**
-     * Determines if empty.
-     *
-     * @return     True if empty, False otherwise.
-     */
-    boolean isEmpty() {
-        return llist.isEmpty();
-    }
-    /**
-     * { function_description }.
-     *
-     * @return     { description_of_the_return_value }
-     */
-    int size() {
-        return llist.size();
-    }
-}
-class Solution {
- public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        Stack st = new Stack();
-        int n = sc.nextInt();
-        sc.nextLine();
-        while (sc.hasNext()) {
-         String str = sc.nextLine();
-         if (str.length() == 0) {
-            System.out.println();
-         }
-         String[] tokens = str.split(" ");
-        switch (tokens[0]) {
-            case "push":
-            st.push(tokens[1]);
-            break;
-            case "enqueue":
-            st.enqueue(tokens[1]);
-            break;
-            case "pop":
-            if(!st.isEmpty()) {
-                System.out.println(st.pop());
+        int tc = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < tc; i++) {
+            Steque st = new Steque();
+            while (sc.hasNext()) {
+                String tmp = sc.nextLine();
+                if (tmp.length() == 0) {
+                    break;
+                }
+                String[] token = tmp.split(" ");
+                switch (token[0]) {
+                case "push":
+                    st.push(Integer.parseInt(token[1]));
+                    st.print();
+                    break;
+                case "enqueue":
+                    st.insertLast(
+                        Integer.parseInt(token[1]));
+                    st.print();
+                    break;
+                case "pop":
+                    st.pop();
+                    st.print();
+                    break;
+                default:
+                    break;
+                }
             }
-            break;
-
-
+            System.out.println();
         }
-
-    }   
-}
+    }
 }
---
+++ ADS-1Practice/m7/7.1 Steque/Steque.java
@@ -0,0 +1,65 @@
+class Steque {
+class Node {
+		int data;
+		Node next;
+	}
+	Node head;
+	int size;
+	void push(int data) {
+		Node current = new Node();
+		size++;
+		if (head == null) {
+			current.data = data;
+			current.next = null;
+			head = current;
+		} else {
+			current.data = data;
+			current.next = head;
+			head = current;
+		}
+	}
+	void pop() {
+		if (size == 0) {
+			return;
+		}
+		Node current = new Node();
+		Node temp = head;
+		temp = temp.next;
+		head.next = null;
+		head = temp;
+		size--;
+	}
+	void insertLast(int data) {
+		Node current = new Node();
+		size++;
+		if (head == null) {
+			current.data = data;
+			current.next = null;
+			head = current;
+		} else {
+			Node temp = head;
+			while (temp.next != null) {
+				temp = temp.next;
+			}
+			current.data = data;
+			temp.next = current;
+			current.next = null;
+		}
+	}
+	void print() {
+		if (size == 0) {
+			System.out.println("Steque is empty.");
+			return;
+		}
+		if (head.next == null) {
+			System.out.println(head.data);
+			return;
+		}
+		Node temp = head;
+		while (temp.next != null) {
+			System.out.print(temp.data + ", ");
+			temp = temp.next;
+		}
+		System.out.println(temp.data);
+	}
+}
