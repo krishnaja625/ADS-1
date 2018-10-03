de8fa4c38a96e4ce278e78058d01e96dfec39372
krishnaja625 <krishnaja625@msitprogram.net>
2018-10-03 16:36:01 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- ADS-1Practice/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-1Practice/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -7,6 +7,12 @@ import java.util.Scanner;
  */
 class Solution {
     /**
+     * Constructs the object.
+     */
+    protected Solution() {
+
+    }
+    /**
      * Function for main.
      *
      * @param      args  The arguments
@@ -14,13 +20,14 @@ class Solution {
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         teamObjects to = new teamObjects();
+        final int t = 3;
         while (sc.hasNext()) {
         String line = sc.nextLine();
         String[] tokens = line.split(",");
         CountryTeams cn = new CountryTeams(
             tokens[0], Integer.parseInt(tokens[1]),
             Integer.parseInt(tokens[2]),
-             Integer.parseInt(tokens[3]));
+             Integer.parseInt(tokens[t]));
                     to.addCountryTeams(cn);
         }
         to.selectionSort();
