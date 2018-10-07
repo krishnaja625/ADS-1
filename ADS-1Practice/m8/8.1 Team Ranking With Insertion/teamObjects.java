c752415caff7a55bcf1dd84ae039547ebd19d4e6
krishnaja625 <krishnaja625@msitprogram.net>
2018-10-07 15:38:15 +0530

    "ads1_m8_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1Practice/m8/8.1 Team Ranking With Insertion/teamObjects.java
+++ ADS-1Practice/m8/8.1 Team Ranking With Insertion/teamObjects.java
@@ -17,16 +17,20 @@ class teamObjects {
 		teamArray = new CountryTeams[10];
 		size = 0;
 	}
-	/* As there is a single line operation the time complexity is constant.
+	/**
+	 * Adds country teams.
+	 * As there is a single line operation the time complexity is constant.
 	 Time complexity = O(1)
-	**/
+	 *
+	 * @param      t     { parameter_description }
+	 */
 	public void addCountryTeams(final CountryTeams t) {
 		teamArray[size++] = t;
 	}
 	/**
 	 * For the for loop it iterates till the size of the array.
 	 * In worst case condition if size is N. Time complexity is O(N).
-	 * 
+	 *
 	 *
 	 * @return     The names.
 	 */
@@ -47,11 +51,13 @@ class teamObjects {
 	public void swaps(final CountryTeams[] team, final int i, final int j) {
 		CountryTeams ct = teamArray[j];
 		teamArray[j] = teamArray[i];
-		teamArray[i] = ct; 
+		teamArray[i] = ct;
 	}
 	/**
 	 * the first for loop iterates for N times,
-	 * the second for loop is independent of 1st for loop and iterates for N-1 times calls the less method whose time complexity is 1.
+	 * the second for loop is independent of 1st for
+	 * loop and iterates for N-1 times calls the less
+	 * method whose time complexity is 1.
 	 * the swap method also takes constant time.
 	 * hence in total iterations are N*(N-1) which is equals to N^2.
 	 * thus the time complexity is N^2
@@ -62,12 +68,11 @@ class teamObjects {
 			for (int j = i + 1; j < size; j++) {
 				if (lessThan(teamArray, j, min)) {
 					min = j;
-				} 
+				}
 			}
 			swaps(teamArray, i, min);
         }
 	}
-
 	/**
 	 * Time complexity is constant.
 	 * Time complexity = O(1)
