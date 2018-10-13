1641e96e8ec1142ce6f9f503c3c45417fc3bacc4
krishnaja625 <krishnaja625@msitprogram.net>
2018-10-13 12:24:18 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1Practice/m18/Week-3_Assignment-1/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Heaping obj = new Heaping();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int j = 0; j < 6; j++){
+            MinPQ<Stocks> minpq = new MinPQ<Stocks>();
+            MaxPQ<Stocks> maxpq = new MaxPQ<Stocks>();
+            for(int i = 0; i <n ; i++) {
+                String[] tokens = sc.nextLine().split(",");
+                Stocks st = new Stocks(tokens[0], Double.parseDouble(tokens[1]));
+                minpq.insert(st);
+                maxpq.insert(st);
+            }
+            String[] strmin = new String[5];
+            for(int z = 0; z < 5; z++) {
+             System.out.println(minpq.delMin().getName());
+               /* strmin[z] = minpq.delMin();*/
+            }
+            // System.out.println(Arrays.toString(strmin));
+            String[] strmax = new String[5];
+            for(int z = 0; z < 5; z++) {
+                // strmax[z] = maxpq.delMax();
+            }
+        }
+    }
+}
---
+++ ADS-1Practice/m18/Week-3_Assignment-1/Stocks.java
@@ -0,0 +1,28 @@
+class Stocks implements Comparable<Stocks> {
+	private String name;
+	private Double perchange;
+	Stocks(String name1, Double perchange1) {
+		this.name = name1;
+		this.perchange = perchange1;
+	}
+	String getName() {
+		return this.name;
+	}
+	Double getperchange() {
+		return this.perchange;
+	}
+
+	public String toString() {
+		return this.name + " " + this.perchange;
+	}
+	
+	public int compareTo(Stocks that) {
+        if(this.perchange > that.perchange) {
+        	return -1;
+        }
+        if( this.perchange < that.perchange) {
+        	return 1;
+        }
+        return this.getName().compareTo(this.getName());
+	}
+}
