983d62beeb4c8905b67c6995368d3ee1793c43bc
krishnaja625 <krishnaja625@msitprogram.net>
2018-10-13 13:09:05 +0530

    "ads1_week3_exam: 1/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1Practice/m18/Week-3_Assignment-1/Solution.java
+++ ADS-1Practice/m18/Week-3_Assignment-1/Solution.java
@@ -35,11 +35,11 @@ final class Solution {
             }
             System.out.println();
             // System.out.println(Arrays.toString(strmin));
-            String[] strmax = new String[5];
+            Stocks[] strmax = new Stocks[5];
             for(int z = 0; z < 5; z++) {
-                // strmax[z] = maxpq.delMax();
+                strmax[z] = maxpq.delMax();
                 // System.out.println(maxpq.delMax().getName());
-                System.out.println(maxpq.delMax());
+                System.out.println(strmax[z]);
             }
             System.out.println();
         }
